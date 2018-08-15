0f2a3d90dc330cc8b38b9de23a51f096717b61d2
Ian Fisher <iafisher@protonmail.com>
2018-08-14 22:24:46 -0400

    Initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+Montague: a natural-language understanding system.
---
+++ montague.py
@@ -0,0 +1,5 @@
+"""A natural-language understanding system.
+
+Author:  Ian Fisher (iafisher@protonmail.com)
+Version: August 2018
+"""
