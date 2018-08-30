d1198a41f5a2f63d6258f8fb641cb065a4172bc3
Ian Fisher <iafisher@protonmail.com>
2018-08-30 11:45:04 -0400

    Augment interactive shell with commands and help message

--- montague/montague.py
+++ montague/montague.py
@@ -14,11 +14,25 @@ from .translator import load_lexicon, translate_sentence
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
+HELP_MESSAGE = '''\
+Available commands:
+    !mode          Display the current operating mode.
+    !mode <mode>   Switch the operating mode.
+    !help          Display this help message.
+    Ctrl+C         Exit the program.
+
+Available modes:
+    translate      Translate English text into logic.
+'''
+AVAILABLE_MODES = {'translate'}
+AVAILABLE_MODES_STR = ', '.join(AVAILABLE_MODES)
+
+
 if __name__ == '__main__':
-    print(
-        'Enter a sentence to see its translation into logic.',
-        'Press Ctrl+C to quit.\n'
-    )
+    print('The Montague natural language system.\n')
+    print(HELP_MESSAGE)
+
+    mode = 'translate'
 
     try:
         with open(os.path.join(FILE_DIR, 'fragment.json')) as f:
@@ -34,10 +48,30 @@ if __name__ == '__main__':
             print()
             break
 
-        try:
-            entry = translate_sentence(formula, lexicon)
-        except Exception as e:
-            print('Error:', e)
+        if formula.startswith('!'):
+            command = formula[1:].rstrip()
+            if command == 'mode':
+                print(f'You are currently in {mode} mode.')
+            elif command.startswith('mode '):
+                new_mode = command.split(maxsplit=1)[1]
+                if new_mode in AVAILABLE_MODES:
+                    mode = new_mode
+                else:
+                    print(
+                        f'{new_mode} is not a recognized mode.',
+                        f'Available modes are: {AVAILABLE_MODES_STR}.'
+                    )
+                    print(f'Remaining in {mode} mode.')
+            elif command == 'help':
+                print(HELP_MESSAGE)
+                print(f'You are currently in {mode} mode.')
+            else:
+                print(f'Unrecognized command {command}.')
         else:
-            print('Denotation:', entry.denotation)
-            print('Type:', entry.type)
+            try:
+                entry = translate_sentence(formula, lexicon)
+            except Exception as e:
+                print('Error:', e)
+            else:
+                print('Denotation:', entry.denotation)
+                print('Type:', entry.type)
