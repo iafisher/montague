a64b5908ce01dc0be00a8646513aba5f7cf561f8
Ian Fisher <iafisher@protonmail.com>
2018-08-30 10:15:01 -0400

    Write basic formula interpreter

---
+++ montague/interpreter.py
@@ -0,0 +1,54 @@
+"""The interpreter that assigns truth values to a logical formula given a model
+of the world.
+
+Author:  Ian Fisher (iafisher@protonmail.com)
+Version: August 2018
+"""
+from collections import namedtuple
+
+from .formula import (
+    AllNode, AndNode, CallNode, ExistsNode, IfNode, OrNode, VarNode,
+)
+
+
+WorldModel = namedtuple('WorldModel', ['individuals', 'assignments'])
+
+
+def interpret_formula(formula, model):
+    if isinstance(formula, VarNode):
+        return model.assignments[formula.value]
+    elif isinstance(formula, AndNode):
+        return interpret_formula(formula.left, model) \
+            and interpret_formula(formula.right, model)
+    elif isinstance(formula, OrNode):
+        return interpret_formula(formula.left, model) \
+            or interpret_formula(formula.right, model)
+    elif isinstance(formula, IfNode):
+        return not interpret_formula(formula.left, model) \
+            or interpret_formula(formula.right, model)
+    elif isinstance(formula, CallNode):
+        caller = interpret_formula(formula.caller, model)
+        arg = interpret_formula(formula.arg, model)
+        return arg in caller
+    elif isinstance(formula, AllNode):
+        old_value = model.assignments.get(formula.symbol)
+        for individual in model.individuals:
+            model.assignments[formula.symbol] = individual
+            if not interpret_formula(formula.body, model):
+                model.assignments[formula.symbol] = old_value
+                return False
+        model.assignments[formula.symbol] = old_value
+        return True
+    elif isinstance(formula, ExistsNode):
+        old_value = model.assignments.get(formula.symbol)
+        for individual in model.individuals:
+            model.assignments[formula.symbol] = individual
+            if interpret_formula(formula.body, model):
+                model.assignments[formula.symbol] = old_value
+                return True
+        model.assignments[formula.symbol] = old_value
+        return False
+    else:
+        # TODO: Handle LambdaNodes differently (they can't be interpreted, but
+        # they should give a better error message).
+        raise Exception('Unhandled class', formula.__class__)
---
+++ test/test_interpreter.py
@@ -0,0 +1,54 @@
+import unittest
+
+from montague.formula import AllNode, AndNode, CallNode, ExistsNode, VarNode
+from montague.interpreter import WorldModel, interpret_formula
+
+
+John = object()
+Mary = object()
+
+
+test_model = WorldModel(
+    set([John, Mary]),
+    {
+        'j': John,
+        'm': Mary,
+        'Good': set([John]),
+        'Bad': set([Mary]),
+        'Human': set([Mary, John]),
+        'Alien': set([]),
+    }
+)
+
+
+class InterpreterTest(unittest.TestCase):
+    def test_john_is_good_is_true(self):
+        formula = CallNode(VarNode('Good'), VarNode('j'))
+        self.assertTrue(interpret_formula(formula, test_model))
+
+    def test_john_is_bad_is_false(self):
+        formula = CallNode(VarNode('Bad'), VarNode('j'))
+        self.assertFalse(interpret_formula(formula, test_model))
+
+    def test_mary_is_bad_and_john_is_good_is_true(self):
+        formula = AndNode(
+            CallNode(VarNode('Bad'), VarNode('m')),
+            CallNode(VarNode('Good'), VarNode('j'))
+        )
+        self.assertTrue(interpret_formula(formula, test_model))
+
+    def test_everyone_is_bad_is_false(self):
+        formula = AllNode('x', CallNode(VarNode('Bad'), VarNode('x')))
+        self.assertFalse(interpret_formula(formula, test_model))
+
+    def test_everyone_is_human_is_true(self):
+        formula = AllNode('x', CallNode(VarNode('Human'), VarNode('x')))
+        self.assertTrue(interpret_formula(formula, test_model))
+
+    def test_someone_is_bad_is_true(self):
+        formula = ExistsNode('x', CallNode(VarNode('Bad'), VarNode('x')))
+        self.assertTrue(interpret_formula(formula, test_model))
+
+    def test_someone_is_alien_is_false(self):
+        formula = ExistsNode('x', CallNode(VarNode('Alien'), VarNode('x')))
+        self.assertFalse(interpret_formula(formula, test_model))
